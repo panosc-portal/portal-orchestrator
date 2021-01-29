88cdd1117c317e74976721605eb917d6e2c86356
Sylvain FEROT <sferot@users.noreply.github.com>
2021-01-29 15:49:24 +0100

    Changed bash expansion to plain if

--- quickstart.sh
+++ quickstart.sh
@@ -6,7 +6,12 @@ if [ ! -f $FILE ]; then
     "Copied 'template.env' to '.env'. Please review the configuration values."
 fi
 
-LOAD_ROUTES=${1:-'-f init-routes.override.yml'}
+#LOAD_ROUTES=${1:-'-f init-routes.override.yml'}
+if [[ -z ${1+x} ]]; then
+    LOAD_ROUTES='-f init-routes.override.yml'
+else
+    LOAD_ROUTES=''
+fi
 COMMAND=${1:-'up'}
 
 docker-compose  -f docker-compose.yml \
